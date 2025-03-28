f0e6eb210ebcfcf94a449ed05c2db494613af3f8
AxelPriet <axeprieto@uniboyaca.edu.co>
2025-03-28 18:45:13 -0500

    Funcion android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.app.ciudades',
+  appName: 'app_ciudades',
+  webDir: 'www'
+};
+
+export default config;
